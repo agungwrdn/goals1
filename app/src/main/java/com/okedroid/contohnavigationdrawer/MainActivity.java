dd4604775a2c31eb175d3c57ecd3c4f4c6e7c8b4
RAW-ON <agungwar00@gmail.com>
2016-11-23 11:20:41 +0700

    EditText onclick DatePicker (fixed)

--- app/src/main/java/com/okedroid/contohnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/okedroid/contohnavigationdrawer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.okedroid.contohnavigationdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -10,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-    //Mendefinisikan variabel
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
@@ -18,25 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Menginisiasi Toolbar dan mensetting sebagai actionbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Menginisiasi  NavigationView
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        //Mengatur Navigasi View Item yang akan dipanggil untuk menangani item klik menu navigasi
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            // This method will trigger on item Click of navigation menu
+
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                //Memeriksa apakah item tersebut dalam keadaan dicek  atau tidak,
                 if(menuItem.isChecked()) menuItem.setChecked(false);
                 else menuItem.setChecked(true);
-                //Menutup  drawer item klik
+
                 drawerLayout.closeDrawers();
-                //Memeriksa untuk melihat item yang akan dilklik dan melalukan aksi
+
                 switch (menuItem.getItemId()){
-                    // pilihan menu item navigasi akan menampilkan pesan toast klik kalian bisa menggantinya
-                    //dengan intent activity
+
                     case R.id.navigation1:
                         Toast.makeText(getApplicationContext(), "Beranda Telah Dipilih", Toast.LENGTH_SHORT).show();
                         return true;
@@ -58,24 +53,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        // Menginisasi Drawer Layout dan ActionBarToggle
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
             @Override
             public void onDrawerClosed(View drawerView) {
-                // Kode di sini akan merespons setelah drawer menutup disini kita biarkan kosong
+
                 super.onDrawerClosed(drawerView);
             }
             @Override
             public void onDrawerOpened(View drawerView) {
-                //  Kode di sini akan merespons setelah drawer terbuka disini kita biarkan kosong
+
                 super.onDrawerOpened(drawerView);
             }
         };
-        //Mensetting actionbarToggle untuk drawer layout
+
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
-        //memanggil synstate
+
         actionBarDrawerToggle.syncState();
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                goHasil();
+            }
+        });
+    }
+
+    private void goHasil() {
+        Intent intent = new Intent(MainActivity.this, InputActivity.class);
+        startActivity(intent);
     }
 
 
